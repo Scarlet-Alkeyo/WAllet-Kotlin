2cd29b0517b04d6f3c5a6b1ce6eeb37b2d96f3aa
Scarlet Alkeyo <scarletalkeyo@gmail.com>
2024-07-08 09:22:29 +0300

    made changes

--- app/src/main/java/com/akirachix/enwallet/MainActivity.kt
+++ app/src/main/java/com/akirachix/enwallet/MainActivity.kt
@@ -6,20 +6,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.akirachix.enwallet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.rvContacts.layoutManager = LinearLayoutManager(this)
+        binding.rvWallet.layoutManager = LinearLayoutManager(this)
         displaywallet()
     }
     fun displayConatcts(){
 
-        val  wallet1 =wallet("Abram","0786752345","@abram.com","")
-        val  wallet2 =wallet("Abram","0786752345","@abram.com","")
-        val  wallet3 =wallet("Abram","0786752345","@abram.com","")
+        val  wallet1 =wallet("Money in","kes 2000","")
+        val  wallet2 =wallet("Money out","kes 3000","")
+        val  wallet3 =wallet("Transaction","kes 400","")
         val  wallet4 =wallet("Abram","0786752345","@abram.com","")
         val  wallet5 =wallet("Abram","0786752345","@abram.com","")
         val  wallet6 =wallet("John","0786457345","@abram.com","")
@@ -31,7 +32,7 @@ class MainActivity : AppCompatActivity() {
 
 
         val walletList = listOf(wallet1,wallet2,wallet3,wallet4,wallet5,wallet6,wallet7,wallet8)
-        val contactAdapter=WalettAdapter(WalletList)
+        val WalettAdapter=WalettAdapter(walletList)
         binding.rvWallet.adapter=WalletAdapter
 
     }
--- app/src/main/java/com/akirachix/enwallet/WalettAdapter.kt
+++ app/src/main/java/com/akirachix/enwallet/WalettAdapter.kt
@@ -2,11 +2,13 @@ package com.akirachix.enwallet
 
 import android.service.quickaccesswallet.WalletCard
 import android.view.LayoutInflater
+import android.view.SurfaceControl.Transaction
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.akirachix.enwallet.databinding.WalletListItemBinding
+import org.w3c.dom.Text
 
 class WalettAdapter {
 }
@@ -24,7 +26,7 @@ override fun onBindViewHolder(holder: WalletViewHolder, position: Int) {
     val wallet=WalletList[position]
     holder.tvBalance.text =wallet.Balance
     holder.tvMoneyOut.text =wallet.MoneyOut
-    holder.tvTransactioin.text=wallet.Transaction
+
 }
 
 
@@ -34,6 +36,13 @@ class WalletViewHolder(itemView: View):
     var MoneyIn=itemView.findViewById<TextView>(R.id.tvMoneyIn )
     var tvMoneyOut = itemView.findViewById<TextView>(R.id.tvMoneyOut)
     var  tvBalance= itemView.findViewById<TextView>(R.id.tvBalance)
+    var Transaction = itemView.findViewById<TextView>(R.id.tvTransaction)
+    var salary = itemView.findViewById<TextView>(R.id.tvSalary)
+    var rent  = itemView .findViewById<TextView>(R.id.tvREnt)
+    var electricity =itemView.findViewById<TextView>(R.id.tvElectricity)
+    var internet =itemView.findViewById<TextView>(R.id.tvINternet)
+    var dividend = itemView.findViewById<TextView>(R.id.tvDividend
+
 
 
 }
